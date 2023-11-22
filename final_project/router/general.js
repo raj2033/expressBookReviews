4e7e84dde3a87ebe03191fd996abd89062174ee7
Raj Sekhar <krj2033@gmail.com>
2023-11-22 01:32:55 -0500

    add async updates

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -16,10 +16,10 @@ public_users.post("/register", (req, res) => {
     const userFiltered = users.filter(user => user.username === username);
     const userExists = userFiltered.length > 0 ? true : false;
 
-    if(userExists){
+    if (userExists) {
         return res.status(400).send("User already exist");
     } else {
-        users.push({username, password});
+        users.push({ username, password });
         return res.status(200).send("User registered successfuly");
     }
 });
@@ -27,32 +27,61 @@ public_users.post("/register", (req, res) => {
 // Get the book list available in the shop
 public_users.get('/', function (req, res) {
     //Write your code here
-    return res.status(200).json({ books: JSON.stringify(books) });
+    async function getBooksAsync() {
+        const promise = new Promise((resolve, reject) => {
+            setTimeout(() => res.status(200).json({ books: JSON.stringify(books) }), 2000);
+        });
+        await promise;
+    }
+    getBooksAsync();
+    // return res.status(200).json({ books: JSON.stringify(books) });
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
     //Write your code here
-    const isbn = req.params.isbn;
-    const book = {};
-    book[isbn] = books[isbn];
-    return res.status(200).json(book);
+    async function getBookByIsbnAsync() {
+        const isbn = req.params.isbn;
+        const book = {};
+        book[isbn] = books[isbn];
+
+        setTimeout(() => res.status(200).send(book), 2000);
+    }
+    getBookByIsbnAsync();
+    // return res.status(200).json(book);
 });
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
     //Write your code here
-    const author = req.params.author;
-    const booksAuthored = Object.values(books).filter(book => book.author === author);
-    return res.status(200).send(booksAuthored);
+    async function getByAuthorAsync() {
+        const promise = new Promise((resolve, reject) => {
+            const author = req.params.author;
+            const book = Object.values(books).filter(book => book.author === author)[0];
+
+            setTimeout(() => res.status(200).send(book), 2000);
+        });
+        await promise;
+    }
+    getByAuthorAsync();
+    // return res.status(200).send(booksAuthored);
 });
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
     //Write your code here
-    const title = req.params.title;
-    const booksFiltered = Object.values(books).filter(book => book.title.toLowerCase().includes(title.toLowerCase()));
-    return res.status(200).send(booksFiltered);
+    async function getByTitleAsync() {
+        const promise = new Promise((resolve, reject) => {
+
+            const title = req.params.title;
+            const booksByTitle = Object.values(books).filter(book => book.title.toLowerCase().includes(title.toLowerCase()));
+
+            setTimeout(() => res.status(200).send(booksByTitle), 2000);
+        });
+        await promise;
+    }
+    getByTitleAsync();
+    // return res.status(200).send(booksFiltered);
 });
 
 //  Get book review
